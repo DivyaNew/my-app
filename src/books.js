d8dae4e461749e44b4b5063e0440a47ad863315b
Divya <divya.halli>
2024-07-05 10:50:11 +0530

    first commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/Book1.jpg';
+import img2 from './images/Book2.webp';
+import img3 from './images/Book3.jpg';
+
+export const books  = [
+    {
+       author:'James Clear',
+       title:'Atomic Habits',
+       img:img1,
+       id:1,
+    },
+    {
+       author:'Jay Shetty',
+       title:'Think like a Monk',
+       img:img2,
+       id:2,
+    },
+    {
+        author:'Ichiro Kishimi and Fumitake Koga ',
+        title:'Courage To Be Disliked',
+        img:img3,
+        id:3,
+     }
+ ];
